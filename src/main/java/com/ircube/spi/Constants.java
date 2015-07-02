33e407b46d359697e4dfb26601fb697708db179f
Administrador <ivo753@gmail.com>
2015-07-02 09:54:57 -0400

    links to api and datastore (localhost)

--- src/main/java/com/ircube/spi/Constants.java
+++ src/main/java/com/ircube/spi/Constants.java
@@ -6,7 +6,7 @@ import com.google.api.server.spi.Constant;
  * Contains the client IDs and scopes for allowed clients consuming your API.
  */
 public class Constants {
-  public static final String WEB_CLIENT_ID = "replace this with your web client ID";
+  public static final String WEB_CLIENT_ID = "1057901676819-26gl5lff79aehu3bbtokur9lt40s9mv5.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
